b8b095b360b338e0a4f02e9301c70770f540ec1a
DESKTOP-5V3FRMH\M <leeyongwoo736@gmail.com>
2025-05-07 17:58:47 +0900

    화면 이동 및 플레이어 이동, 플레이어 애니메이션 구현

---
+++ Assets/My2D/Scripts/AnimationString.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+namespace My2D
+{
+    // 애니메이션 파라미터를 정의하는 클래스
+    public class AnimationString
+    {
+        public static string isMoving = "IsMoving";
+        public static string isRunning = "IsRunning";
+    }
+}
